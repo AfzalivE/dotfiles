c9a18a47928e196972dd56ea34e8be63ea4eedd5
Afzal Najam <AfzalivE@users.noreply.github.com>
2017-04-05 15:01:22 -0400

    faster nvm

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,6 @@
 alias rm='rm -i'
 alias gdw='./gradlew'
 alias gdw-refresh='gdw --refresh-dependencies'
+
+export NVM_DIR="/Users/afzal/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use  # This loads nvmeval $(/usr/libexec/path_helper -s)
